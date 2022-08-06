ccd2ebdd55d84d6603716e15d3a93ae61162e026
Patrick Woodcock <patrick@patrickw.tech>
2022-08-06 12:23:28 +0100

    first commit

---
+++ games-list.js
@@ -0,0 +1,50 @@
+const games = [
+    {
+        name: 'Clock Watcher',
+        url: 'https://patrick64.github.io/clock-watcher/',
+        release: '2017-06-22',
+        platform: '🖥️ 📱 Play in browser for desktop or tablet',
+        description: `Made for a work themed game jam. It's made up of some <a href="https://www.youtube.com/watch?v=Qm49HqGuYvg" target="_blank">office olympics</a>
+            style mini-games where you have to make it through the work day. Win an office chair race, prop eyes open during a boring meeting, collect all the tea, jump over miserable coworkers to get the cake and beat up a printer.`,
+        instructions: `On desktop use the arrow keys to move around/jump or beat up the printer. On tablet you can tap and drag to move around. 
+        If you win a round the clock goes forward. You need to make it to 5pm.`,
+        reception: `After submitting this to game jam it went unnoticed and the game jam shut down shortly after.
+        I forced a colleague at work to play it, he let out a groan of pain when he had to replay the pacman level`,
+        image: 'clock-watcher.jpg',
+    },
+    {
+        name: 'Timeless',
+        url: 'https://jimmynoshoes.itch.io/timeless',
+        release: '2016-03-10',
+        platform: '😔 See it not working',
+        description: `A story based game that uses google streetview. It doesn't work anymore as google have updated 
+        streetview for the areas it covers. When it did work it used dialogue boxes for the story and ghouls would chase you. The ghouls were actually
+        maps markers in streetview. Made for the Nar8 game jam.`,
+        instructions: `It doesn't work anymore sorry`,
+        reception: `This got <a href="https://itch.io/jam/nar8-jam/results" target="_blank">second place</a> in the game jam!`,
+        image: 'timeless.png'
+    },
+    {
+        name: 'Space Suckers',
+        url: 'https://jimmynoshoes.itch.io/space-suckers',
+        release: '2017-12-02',
+        platform: '🖥️ Play in browser for desktop only',
+        description: `Space monsters try to grab onto your space ship and you have to drop them in black holes to avoid
+        being dragged down.  Made for Ludum Dare game jam. I had help from my housemates Tim with coding and Pieta who did the lovely artwork. `,
+        instructions: 'Instructions on intro screen.',
+        reception: 'Overall it came 501st out of 1846 entries so not bad.',
+        image: 'space-suckers.png'
+    },
+    {
+        name: 'Precious Things',
+        url: 'https://jimmynoshoes.itch.io/precious-things',
+        release: '2018-08-12',
+        platform: 'Play in browser for desktop only',
+        description: `Your house is shrinking and you have pile objects on top of each other jenga style to make sure they don't get crushed.
+        Made for Ludum Dare game jam over 48 hours which had the theme 'Running out of space' using Construct2. I was going to do something
+        much more ambitious but then settled on this simple game. `,
+        instructions: 'Drag and drop objects.',
+        reception: 'It came in <a href="https://ldjam.com/events/ludum-dare/42/precious-things" target="_blank">437th out of 2013</a>, and for the theme it came in at 89th.',
+        image: 'precious-things.png'
+    }
+]
